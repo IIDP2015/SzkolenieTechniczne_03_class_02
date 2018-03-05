257a34f6556141a64334fd391e84b5f4917f276a
Szczepan Zaskalski <szaskalski@gmail.com>
2018-03-05 19:10:05 +0100

    Added ProductResponse

---
+++ MVC/Models/ProductResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class ProductResponse
+    {
+        [Required(ErrorMessage = "Proszę podać imie i nazwisko")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Proszę podać adres email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Proszę podać numer telefonu")]
+        public string Phone { get; set; }
+        public bool? New { get; set; }
+    }
+}
